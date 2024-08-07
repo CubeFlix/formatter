e04a0d1de9069d8193b825ebb41a54c4179d474f
cubeflix <kevin.signal@gmail.com>
2024-08-06 21:29:49 -0500

    add soft hyphen (for some reason not added in previous commit)

---
+++ src/main/java/com/cubeflix/formatterapp/SoftHyphen.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cubeflix.formatterapp;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Kevin Chen
+ */
+public class SoftHyphen implements InlineObject {
+    SoftHyphen() {
+        
+    }
+    
+    public float getHeight() throws IOException {
+        return 0.0f;
+    }
+    
+    public float getWidth() throws IOException {
+        return 0.0f;
+    }
+}
